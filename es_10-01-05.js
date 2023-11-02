f2d469f6e1bbc2325884760a6a086e0e2715fc03
Daniele <daniava2001@gmail.com>
2023-11-02 15:43:57 +0100

    Nuovi esercizi

---
+++ es_10-01-05.js
@@ -0,0 +1,10 @@
+let firstNumber = 4; 
+let secondNumber = 7;
+let thirdNumber = 13;
+let fourthNumber = 1; 
+let fifthNumber = 18; 
+
+let sumNumbers = firstNumber + secondNumber + thirdNumber + fourthNumber + fifthNumber; 
+let avrNumbers = sumNumbers / 5; 
+
+console.log(sumNumbers, avrNumbers); 
